a8f45f3c6d254cb3a723cdacc02ebefa60474f6d
ychen <ychen@thorlabs.com>
2016-06-22 18:07:24 +0800

    ...

--- GetMp3.py
+++ GetMp3.py
@@ -2,7 +2,9 @@ import requests
 import urllib
 r = requests.get('http://music.163.com/api/playlist/detail?id=3779629')
 arr = r.json()['result']['tracks']
-for i in range(10): name = str(i+1)+ ' ' + arr[i]['name']+ '.mp3'
-link = arr[i]['mp3Url']
-urllib.request.urlretrieve(link,'E:/PythonScripts/'+name)
+for i in range(10): 
+	name = str(i+1)+ ' '+ '.mp3'
+	link = arr[i]['mp3Url']
+	print(link)
+	urllib.request.urlretrieve(link,'E:/MyPythonScripts/'+name)
 print(name+'Download successfully!')
--- GetPic.py
+++ GetPic.py
@@ -1,23 +1,24 @@
 #coding=utf-8
 import urllib.request
 import re
-
+PageNumber =1
 def getHtml(url):
     page = urllib.request.urlopen(url)
     html = page.read()
     return html
 
 def getImg(html):
-    reg = r'src="(.+?\.png)"'
+    reg = r' <img src="(.*?.png)" alt='
     imgre = re.compile(reg)
     html = html.decode('utf-8')
     imglist = re.findall(imgre,html)
     x = 0
     for imgurl in imglist:
-        urllib.request.urlretrieve(imgurl,'%s.png' % x)
+        print(imgurl)
+        urllib.request.urlretrieve(imgurl,'E:\MyPythonScripts\Icons'+str(PageNumber)+'%s.png' % x)
         x+=1
-
-
-html = getHtml("http://findicons.com/search/home")
-
-print(getImg(html))
+for i in range(20):
+	html = getHtml("http://findicons.com/search/home/"+str(i))
+	getImg(html)
+	PageNumber+=1
+	
